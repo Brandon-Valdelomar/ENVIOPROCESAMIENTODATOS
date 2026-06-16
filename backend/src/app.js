f25fd15448fd50dbeed88b9033662f25c4ae0e77
Student <brvaldelomarco@est.utn.ac.cr>
2026-06-16 11:02:07 -0600

    Prueba con postman y retornar el objeto
    APIS CALCULAR FUNCIONA CORRECTAMENTE

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ const PORT=process.env.SERVER_PORT;
 const VERSION=process.env.SERVER_VERSION;
 const DESCRIPTION=process.env.SERVER_DESCRIPTION;
 
+
 const app = express();
 
 app.use(cors());
@@ -27,3 +28,41 @@ app.listen(4000,()=>{
     console.log('Servidor funcionando correctamente en http://localhost:4000');
 });
 
+app.post("/api/parqueo/calcular", (req,res)=>{
+const {placa,tipo,horas,minutos} = req.body;
+
+ if(!placa ||placa.trim()==""){
+    res.status(400).json({error:"Placa obligatoria"})
+ }
+
+if(!tipo ||tipo!=="carro" && tipo!=="moto"){
+res.status(400).json({error:"Solo se permite carro o moto"})
+}
+
+if(horas < 0 ){
+    res.status(400).json({error:"Las horas no pueden ser negativas"})
+}
+
+if(minutos < 0 || minutos > 59 ){
+    res.status(400).json({error:"Las horas no pueden ser negativas y no pueden ser mayores a 59"})
+}
+
+const tarifa=tipo==="carro"? 1200: 500;
+
+let h=Number(horas);
+let m=Number(minutos);
+
+if(m>5)h++;
+
+const total=h*tarifa;
+
+res.json({
+    placa:placa,
+    tipo:tipo,
+    taria:tarifa,
+    tiempo:horas+":"+minutos,
+    horasCobradas:h,
+    total:total
+})
+
+});
